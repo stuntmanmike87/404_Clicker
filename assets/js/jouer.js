10ff2c11cfad118b949df07fc80488acd19a7ac6
alexoui87 <ptitalex87000@gmail.com>
2022-03-31 15:16:20 +0200

    modif js

--- assets/js/jouer.js
+++ assets/js/jouer.js
@@ -1,13 +1,45 @@
 window.onload = () => {
+    /*=====================================================================================
+	INITIALISATION
+	=======================================================================================*/
     let Game = {
         score : document.getElementById("score"),
         image : document.getElementById("image"),
         scoreJS : 0,
         incrementeur: 1,
     }
-
+	/*=====================================================================================
+	VERIF DES POINTS USER
+	=======================================================================================*/
+    /*=====================================================================================
+	FONCTION CLIC / SAVE DE GAME
+	=======================================================================================*/
     image.addEventListener("click", function(){
         Game.scoreJS = Game.scoreJS + Game.incrementeur;
         score.innerHTML = "<p>Le nombre d'erreur(s) est de \n" + Game.scoreJS + "</p>";
+		localStorage.getItem(Game.scoreJS);
     })
+	/*=====================================================================================
+	PASSAGE DES LEVELS
+	=======================================================================================*/
+    /*=====================================================================================
+	TEST ANTI-CHEAT
+	=======================================================================================*/
+    if (!Game.ready)
+	{
+		if (top!=self) Game.ErrorFrame();
+		else
+		{
+			console.log('[=== '+choose([
+				'Oh, hello!',
+				'hey, how\'s it hangin',
+				'About to cheat in some cookies or just checking for bugs?',
+				'Remember : cheated cookies taste awful!',
+				'Hey, LAG here. Cheated error destroy pc... or do they?',
+			])+' ===]');
+			Game.Load();
+			//try {Game.Load();}
+			//catch(err) {console.log('ERROR : '+err.message);}
+		}
+	}
 }
