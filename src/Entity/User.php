1380a7cf87b5505aaeb34a8a23488b25b7003e1a
mick-us <mickou147@gmail.com>
2022-04-06 16:58:02 +0200

    ajout de CRUD pour la gestion du profil

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -98,7 +98,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUsername(): string
     {
-        return (string) $this->email;
+        return (string) $this->username;
     }
 
     /**
