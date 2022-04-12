d58c5634c4a82360774afa5658c89122d5ec3b31
alexoui87 <ptitalex87000@gmail.com>
2022-04-12 14:14:36 +0200

    ajout changement de mot de passe

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -37,7 +37,7 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user);
-            return $this->redirectToRoute('app_user_show', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_user_show', ['id' => $user->getID()], Response::HTTP_SEE_OTHER);
         }
 
         return $this->renderForm('user/edit.html.twig', [
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,15 +6,43 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('password')    
-        ;
+        ->add('plainPassword', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'first_options' => [
+                'attr' => ['autocomplete' => 'new-password'/**,'class' => 'form-control' */],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'label' => 'Nouveau mot de passe',
+            ],
+            'second_options' => [
+                'attr' => ['autocomplete' => 'new-password'],
+                'label' => 'Saisir à nouveau le mot de passe',
+            ],
+            'invalid_message' => 'Les mots de passe doivent correspondre.',
+            // Instead of being set onto the object directly,
+            // this is read and encoded in the controller
+            'mapped' => false,
+        ])
+    ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
