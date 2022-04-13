4a9b5c3cd49aeb07b0f97b0d0139396303a726f9
Miriam <miriamt.simonnet@gmail.com>
2022-04-13 08:55:38 +0000

    Changement de level si score atteint WIP

--- assets/js/jouer.js
+++ assets/js/jouer.js
@@ -6,7 +6,7 @@ let game = {
 	saveButton: document.querySelector('.savePoints'),
 	scoreJS: parseFloat(score.dataset.userPoints),
     incrementeur: 1,
-	changeLevelScore: 100,
+	changeLevelScore: parseInt(score.dataset.userLevel),
 	url: "http://localhost:1234/save_axios",
 
 	init: function() {
@@ -25,7 +25,7 @@ let game = {
 			game.postData(game.scoreJS);
 		}
 
-		// game.saveGame();
+		game.saveGame();
 	},
 
 	saveGame: function() {
@@ -59,70 +59,4 @@ let game = {
  	}
 }
 document.addEventListener('DOMContentLoaded', game.init);
-// const axios = require('axios');
-// window.onload = () => {
-//     /*=====================================================================================
-// 	INITIALISATION
-// 	=======================================================================================*/
-//     let Game = {
-//         score : document.getElementById("score"),
-//         image : document.getElementById("image"),
-//         scoreJS : 0,
-//         incrementeur: 1,
-// 		url: "http://localhost:1234/",
-//     }
-// 	/*=====================================================================================
-// 	VERIF DES POINTS USER
-// 	=======================================================================================*/
-//     /*=====================================================================================
-// 	FONCTION CLIC / SAVE DE GAME
-// 	=======================================================================================*/
-//     image.addEventListener("click", function(){
-//         Game.scoreJS = Game.scoreJS + Game.incrementeur;
 
-// 		// https://stackoverflow.com/questions/47817325/storing-my-game-score-in-local-storage
-// 		localStorage.setItem("score", Game.scoreJS);
-
-// 		// Get data from localStorage
-// 		let score1 = localStorage.getItem("score");
-// 		console.log(score1);
-
-//         Game.score.innerHTML = "<p>Le nombre d'erreur(s) est de \n" + Game.scoreJS + "</p>";
-
-// 		// https://blog.logrocket.com/using-axios-set-request-headers/
-// 		function sendData(data) {
-// 			axios.post(Game.url, {
-// 				data: data
-// 			})
-// 			.then(res => console.log(res))
-// 			.catch(err => console.log(err));
-
-//  		}
-// 		sendData(score1);
-//     })
-	
-	
-
-	/*=====================================================================================
-	PASSAGE DES LEVELS
-	=======================================================================================*/
-    /*=====================================================================================
-	TEST ANTI-CHEAT
-	=======================================================================================*/
-    // if (!Game.ready)
-	// {
-	// 	if (top!=self) Game.ErrorFrame();
-	// 	else
-	// 	{
-	// 		console.log('[=== '+choose([
-	// 			'Oh, hello!',
-	// 			'hey, how\'s it hangin',
-	// 			'About to cheat in some cookies or just checking for bugs?',
-	// 			'Remember : cheated cookies taste awful!',
-	// 			'Hey, LAG here. Cheated error destroy pc... or do they?',
-	// 		])+' ===]');
-	// 		Game.Load();
-	// 		//try {Game.Load();}
-	// 		//catch(err) {console.log('ERROR : '+err.message);}
-	// 	}
-	// }
