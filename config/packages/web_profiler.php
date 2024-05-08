218f38eaec0534c6a3e3146916cf63a0ee66e45c
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-08 18:01:27 +0200

    Update sf/web-profiler-bdle recipe

--- config/packages/web_profiler.php
+++ config/packages/web_profiler.php
@@ -6,11 +6,37 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     if ($containerConfigurator->env() === 'dev') {
-        $containerConfigurator->extension('web_profiler', ['toolbar' => true, 'intercept_redirects' => false]);
-        $containerConfigurator->extension('framework', ['profiler' => ['only_exceptions' => false]]);
+        $containerConfigurator->extension(
+            'web_profiler',
+            [
+                'toolbar' => true,
+                'intercept_redirects' => false
+            ]);
+        $containerConfigurator->extension(
+            'framework',
+            [
+                'profiler' => [
+                    'only_exceptions' => false,
+                    'collect_serializer_data' => true
+                ]
+            ]
+        );
     }
     if ($containerConfigurator->env() === 'test') {
-        $containerConfigurator->extension('web_profiler', ['toolbar' => false, 'intercept_redirects' => false]);
-        $containerConfigurator->extension('framework', ['profiler' => ['collect' => false]]);
+        $containerConfigurator->extension(
+            'web_profiler',
+            [
+                'toolbar' => false,
+                'intercept_redirects' => false
+            ]
+        );
+        $containerConfigurator->extension(
+            'framework',
+            ['profiler' => 
+                [
+                    'collect' => false
+                ]
+            ]
+        );
     }
 };
