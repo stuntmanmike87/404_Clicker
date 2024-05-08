6722b70d27f0bd16267b298b6c27afde870294e2
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-08 18:07:55 +0200

    Update sf/twig-bdle recipe

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension('twig', ['default_path' => '%kernel.project_dir%/templates', 'form_themes' => ['bootstrap_5_layout.html.twig']]);
+    $containerConfigurator->extension('twig', ['file_name_pattern' => '*.twig', 'form_themes' => ['bootstrap_5_layout.html.twig']]);
 };
