6ab4bc953d2afa875fe4ac66d301f425912b3c8d
stuntmanmike87 <mich.perigaud@gmail.com>
2024-05-31 15:20:35 +0200

    Update recipe for Doctrine bundle

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -11,16 +11,22 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             'url' => '%env(resolve:DATABASE_URL)%'],
             'orm' => [
                 'auto_generate_proxy_classes' => true,
+                //'enable_lazy_ghost_objects' => true,
+                //'validate_xml_mapping' => true,
                 'naming_strategy' => 'doctrine.orm.naming_strategy.underscore_number_aware',
                 'auto_mapping' => true,
                 'mappings' => [
                     'App' => [
+                        //'type' => 'attribute',
                         'is_bundle' => false,
                         //'type' => 'annotation',
                         'dir' => '%kernel.project_dir%/src/Entity',
                         'prefix' => 'App\Entity',
                         'alias' => 'App'
                     ]
+                    ],
+                'controller_resolver' => [
+                    'auto_mapping' => false,
                 ]
             ]
         ]
